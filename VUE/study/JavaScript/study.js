87a14648a53c0cc2db14c2bcaee24f04ae2ff2d1
yuwencool <1178498474@qq.com>
2022-05-14 22:34:24 +0800

    Update study.js

--- VUE/study/JavaScript/study.js
+++ VUE/study/JavaScript/study.js
@@ -63,6 +63,36 @@ Vue.component ('school', {
 Vue.component ('dorm', {
     template: '<div>这是宿舍</div>'
 })
+//使用Vue.directive演示自定义组件的代码实现
+Vue.directive('myaction', {
+    //当绑定元素插到DOM中，元素自动获得焦点
+    inserted (el, binding) {
+        //有两个参数，其中el表示当前自定义组件的元素，binding表示指令的相关信息
+        //这里的binding.value表示标签v-mycation的值，如果是true就获得焦点
+        if (binding.value) {
+            el.focus();
+        }
+    }
+})
+
+//Vue.use的主要作用是在Vue中安装插件，通过插件可以为Vue添加全局功能。插件可以是一个对象或者函数。如果是对象必须提供install()方法，用来安装插件。如果是函数，则该函数被当作install()方法
+//定义一个MyPlugin插件对象
+let MyPlugin = {};
+//给插件定义安装函数，其中包括两个参数，第一个参数Vue是Vue的构造器，第二个参数options是一个可选的配置对象
+MyPlugin.install = function (Vue, options) {
+    console.log(options);
+    //在插件中给Vue添加自定义指令
+    Vue.directive('my-directive', {
+        bind (el, binding) {
+            //为自定义指令v-my-directive绑定的DOM元素设置style样式
+            el.style = 'width: 100px; height = 100px'
+        }
+    })
+}
+Vue.use(MyPlugin, {someOption: true});
+var vm1 = new Vue({
+    el: '#app1'
+})
 var vm = new Vue({
     el: '#app',
     data: {
@@ -114,7 +144,46 @@ var vm = new Vue({
 
 var viewmodel = new Vue({
     el: '#software',
-    data: {comName: 'school'}
+    data: {
+        comName: 'school',
+        uname: '学习者'
+    },
+    /*钩子函数过程：创建实例->页面挂载->组件更新->实例销毁（VUE生命周期）
+    创建实例之前：不可以使用数据
+    创建实例之后：可以使用数据
+    页面挂载之前：不可以展示数据
+    页面挂载之后：可以展示数据
+    组件更新之前：
+    组件更新之后：
+    实例销毁之前：
+    实例销毁之后：无法操作DOM元素
+    */
+    beforeCreat () {
+        //此时数据不能使用
+        console.log('创建实例之前：', this.$data.uname);
+    },
+    created () {
+        //此时数据可以使用
+        console.log('创建实例之后：', this.$data.uname);
+    },
+    beforeMounted () {
+        console.log('挂载页面之前：', this.$el.innerHTML);
+    },
+    mounted () {
+        console.log('挂载页面之后：', this.$el.innerHTML);
+    },
+    beforeUpdate () {
+
+    },
+    updated () {
+
+    },
+    beforeDestroy () {
+
+    },
+    destroyed () {
+        
+    }
 })
 
 console.log(vm.$data.msg)
