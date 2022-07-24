66366044b5334b4e6aa2faf3115e21e7dbfa5738
yuwencool <1178498474@qq.com>
2022-07-24 23:35:25 +0800

    添加“VUE动画”学习

---
+++ VUE/animation/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <animaTest/>
+  </div>
+</template>
+
+<script>
+import animaTest from './components/animaTest.vue';
+
+export default {
+  name: 'App',
+  components: {
+    animaTest
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ VUE/animation/src/components/animaTest.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <transition appear>
+      <h1 v-show="isShow">你好</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'animaTest',
+    data() {
+      return {
+        isShow: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  h1 {
+    background-color: orange;
+  }
+  .v-enter-active {
+    animation: dida 1s linear;
+  }
+  .v-leave-active {
+    animation: dida 1s linear reverse;
+  }
+  @keyframes dida {
+    from {
+      transform: translateX(-100%);
+    }
+    to {
+      transform: translateX(0px);
+    }
+  }
+</style>>
