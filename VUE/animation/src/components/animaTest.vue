9cf729a56687c5684431225f2399a5e715cc0742
yuwencool <1178498474@qq.com>
2022-07-27 00:37:51 +0800

    添加动画的其他写法

--- VUE/animation/src/components/animaTest.vue
+++ VUE/animation/src/components/animaTest.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
     <button @click="isShow = !isShow">显示/隐藏</button>
-    <transition appear>
+    <transition name="hello" appear>
       <h1 v-show="isShow">你好</h1>
     </transition>
+    <transition name="test" appear>
+      <h1 v-show="isShow">你好2</h1>
+    </transition>
+    <!-- appear可以使得元素初始加载也有动画 -->
+    <!-- transition里面只能包裹一个子节点，多个时需要使用transitionGroup -->
+    <transition-group appear>
+      <h1 v-show="!isShow" key="1">你好3</h1>
+      <h1 v-show="isShow" key="2">你好4</h1>
+    </transition-group>
   </div>
 </template>
 
@@ -21,11 +30,12 @@
 <style scoped>
   h1 {
     background-color: orange;
+    transition: 1s linear;
   }
-  .v-enter-active {
+  .hello-enter-active {
     animation: dida 1s linear;
   }
-  .v-leave-active {
+  .hello-leave-active {
     animation: dida 1s linear reverse;
   }
   @keyframes dida {
@@ -35,5 +45,13 @@
     to {
       transform: translateX(0px);
     }
+  }
+
+  /* 下面是另一种写法 */
+  .test-enter, .test-leave-to {
+    transform: translateX(-100%);
+  }
+  .test-leave, .test-enter-to {
+    transform: translateX(0px);
   }
 </style>>
