2e87c0a19abb6c4681c7b756154f2ed287c56237
yuwencool <1178498474@qq.com>
2022-07-27 22:59:37 +0800

    添加动画

--- VUE/animation/src/App.vue
+++ VUE/animation/src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <animaTest/>
+    <anima-test-1/>
   </div>
 </template>
 
 <script>
 import animaTest from './components/animaTest.vue';
+import AnimaTest1 from './components/animaTest1.vue';
 
 export default {
   name: 'App',
   components: {
-    animaTest
+    animaTest,
+    AnimaTest1,
+
   }
 }
 </script>
---
+++ VUE/animation/src/components/animaTest1.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <!-- appear可以使得元素初始加载也有动画 -->
+    <!-- transition里面只能包裹一个子节点，多个时需要使用transitionGroup -->
+    <transition-group 
+      name="animate__animated animate__bounce"
+      appear
+      enter-active-class="animate__jello"
+      leave-active-class="animate__backOutDown"
+    >
+      <h1 v-show="!isShow" key="1">你好3</h1>
+      <h1 v-show="isShow" key="2">你好4</h1>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+  import 'animate.css';
+  export default {
+    name: 'animaTest1',
+    data() {
+      return {
+        isShow: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  h1 {
+    background-color: orange;
+  }
+</style>>
